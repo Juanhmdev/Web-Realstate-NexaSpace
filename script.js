52beabf5430b91c776f457bc779483711e266507
Juanhmdev <Juanandres070502@gmail.com>
2024-05-07 12:53:10 -0500

    segundo avance de la web

--- script.js
+++ script.js
@@ -16,3 +16,50 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     menu.classList.remove("active");
 });
+
+// swiper 
+const swiper = new Swiper (".swiper", {
+    slidesPerView: 3,
+    direction: "horizontal",
+    lazyLoading: true,
+    spaceBetween: 7,
+    grabCursor: false,
+    centeredSlides: true,
+    loop:true,
+    keyboard: {
+        enabled: true,
+    },
+
+    // responsive breakpoints
+
+    breakpoints: {
+        320: {
+            slidesPerView:1,
+            spaceBetween: 30
+        },
+
+        700:{
+            slidesPerView:1.5,
+            spaceBetween:7
+        },
+
+        768:{
+            slidesPerView:1.5,
+            spaceBetween:10,
+        },
+        1024: { 
+            slidesPerView:3,
+            spaceBetween:10,
+        },
+        1440: {
+            slidesPerView:3,
+            spaceBetween:10,
+        },
+    },
+
+    // navigation arrows
+    navigation:{
+        nextEl: ".swiper-custom-nav-next",
+        prevEl: ".swiper-custom-nav-prev",
+    }
+});
